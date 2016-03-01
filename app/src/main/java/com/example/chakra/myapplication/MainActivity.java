acf24e8fe1d5deaff74b68f4126afbc92b3164b1
chakradharGitHub <chakradhar.avala@gmail.com>
2016-02-29 21:16:00 -0500

    commit at 09:15pm 02/29

--- app/src/main/java/com/example/chakra/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chakra/myapplication/MainActivity.java
@@ -18,55 +18,73 @@ public class MainActivity extends AppCompatActivity {
     {
         EditText a1 = (EditText) findViewById(R.id.TFnumber1);
         EditText a2 = (EditText) findViewById(R.id.TFnumber2);
-        double n1, n2, res=0;
+        double n1, n2, res;
         n1 = Double.parseDouble(a1.getText().toString());
         n2 = Double.parseDouble(a2.getText().toString());
         TextView t = (TextView) findViewById(R.id.TVresult);
 
-        if(v.getId() == R.id.Badd)
-        {
-            res=n1+n2;
-            t.setText(""+res);
+
+
+    if (v.getId() == R.id.Badd) {
+
+        if ((a1 != null) && (a2 != null)) {
+            res = n1 + n2;
+            t.setText("" + res);
+        } else {
+            t.setText("Please provide required input...");
         }
-       if (v.getId() == R.id.Bsub)
-        {
+    }
+    if (v.getId() == R.id.Bsub) {
+        if ((a1 == null) || (a2 == null))
+            t.setText("Please provide required input...");
+        else {
             if (n1 > n2)
-              res = n1-n2;
+                res = n1 - n2;
             else
-              res = n2-n1;
-            t.setText(""+res);
+                res = n2 - n1;
+            t.setText("" + res);
         }
-       if (v.getId() == R.id.Bmul)
-        {
-            res = n1*n2;
-            t.setText(""+res);
+    }
+    if (v.getId() == R.id.Bmul) {
+        if ((a1 == null) || (a2 == null))
+            t.setText("Please provide required input...");
+        else {
+            res = n1 * n2;
+            t.setText("" + res);
         }
-        if (v.getId() == R.id.Bdiv)
-        {
-            if(n1>n2) {
+    }
+    if (v.getId() == R.id.Bdiv) {
+        if ((a1 == null) || (a2 == null))
+            t.setText("Please provide required input...");
+        else {
+            if (n1 > n2) {
                 res = n1 / n2;
                 t.setText("" + res);
-            }
-            else
+            } else
                 t.setText("Can NOT be Divided !");
         }
-        if (v.getId()== R.id.Bsqr) {
-            try {
+    }
+    if (v.getId() == R.id.Bsqr) {
+
+        try {
+            if ((a1 == null) || (a2 == null))
+                t.setText("Please provide required input...");
+            else {
                 res = n1;
                 for (int x = 2; x <= n2; x++)
                     res *= n1;
                 t.setText("" + res);
-            }catch (Exception e) {
-                t.setText(e.getMessage());
             }
+        } catch (Exception e) {
+            t.setText(e.getMessage());
+        }
 
-        }
-        if(v.getId()==R.id.Bclr)
-        {
-           a1.setText("");
-            a2.setText("");
-            t.setText("");
-        }
+    }
+    if (v.getId() == R.id.Bclr) {
+        a1.setText("");
+        a2.setText("");
+        t.setText("");
+    }
 
 
 
